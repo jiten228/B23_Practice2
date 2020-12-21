64f1030af3ec051a42c26056cfcbd276fff5996a
jiten228 <jiten228@yahoo.com>
2020-12-21 17:54:33 -0600

    Check head frequently

--- src/Jitendra_2021/Jitendra_001.java
+++ src/Jitendra_2021/Jitendra_001.java
@@ -3,5 +3,6 @@ package Jitendra_2021;
 public class Jitendra_001 {
     public static void main(String[] args) {
         System.out.println("Hello Guys, new practice on github");
+        System.out.println("Check head frequently");
     }
 }
