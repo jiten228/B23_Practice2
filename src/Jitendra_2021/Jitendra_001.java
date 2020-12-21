cee47633128cc32c047262d4068d05d0d7c800b2
jiten228 <jiten228@yahoo.com>
2020-12-21 17:59:58 -0600

    Create a new branch under your last name

--- src/Jitendra_2021/Jitendra_001.java
+++ src/Jitendra_2021/Jitendra_001.java
@@ -4,5 +4,6 @@ public class Jitendra_001 {
     public static void main(String[] args) {
         System.out.println("Hello Guys, new practice on github");
         System.out.println("Check head frequently");
+        System.out.println("Create a new branch under your last name");
     }
 }
