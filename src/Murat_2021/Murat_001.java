606341331cf1e168397f7dabe5b83c0fe1e38757
Murat Guney <muratguney@msn.com>
2020-12-21 22:26:47 -0500

    branch change

--- src/Murat_2021/Murat_001.java
+++ src/Murat_2021/Murat_001.java
@@ -7,6 +7,7 @@ public class Murat_001 {
         System.out.println("How is your going?");
         System.out.println("I created new branch");
         System.out.println("But I did it new branch ");
+        System.out.println(" I hope, Is this way right?" );
     }
 }
 
