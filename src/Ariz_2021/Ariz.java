6b429cf194a61ddb0c3f4d02c726f5d98d46f1dd
Ariz <74077376+Ariz023@users.noreply.github.com>
2020-12-22 16:46:08 -0600

    added

---
+++ src/Ariz_2021/Ariz.java
@@ -0,0 +1,16 @@
+package Ariz_2021;
+
+public class Ariz {
+
+    public static void main(String[] args) {
+
+        System.out.println("Check head frequently");
+        System.out.println("Observe head lower right corner it should be on Branch");
+        System.out.println("Bank of America");
+        System.out.println("Head should be on branch & push the code");
+
+
+    }
+
+
+}
