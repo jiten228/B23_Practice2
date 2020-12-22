cae02970588fb4efb1e14b8446265311e55e6a82
jiten228 <jiten228@yahoo.com>
2020-12-21 18:03:27 -0600

    Head should be on branch & push the code

--- src/Jitendra_2021/Jitendra_001.java
+++ src/Jitendra_2021/Jitendra_001.java
@@ -7,5 +7,6 @@ public class Jitendra_001 {
         System.out.println("Create a new branch under your last name");
         System.out.println("Observe head lower right corner it should be on Branch");
         System.out.println("AAAAAAAAAAAAAAA");
+        System.out.println("Head should be on branch & push the code");
     }
 }
