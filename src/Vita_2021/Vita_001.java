4c0a49b3a3290144f2e3d12e2e29bc5125f4e050
Vitakachun <kachunvita@gmail.com>
2020-12-21 20:49:55 -0600

    added more for this project

--- src/Vita_2021/Vita_001.java
+++ src/Vita_2021/Vita_001.java
@@ -4,5 +4,6 @@ public class Vita_001 {
     public static void main(String[] args) {
         System.out.println("Hi, everyone!");
         System.out.println("How are you? Let's practice our new project");
+        System.out.println("Wow! We are doing amazing job!");
     }
 }
