2e18d46e870cee464026b3d29260b2f307db10fe
Vitakachun <kachunvita@gmail.com>
2020-12-21 20:54:27 -0600

    added more code to the project

--- src/Vita_2021/Vita_001.java
+++ src/Vita_2021/Vita_001.java
@@ -6,5 +6,6 @@ public class Vita_001 {
         System.out.println("How are you? Let's practice our new project");
         System.out.println("Wow! We are doing amazing job!");
         System.out.println("Created new branch");
+        System.out.println("Trying to do half of the project today");
     }
 }
