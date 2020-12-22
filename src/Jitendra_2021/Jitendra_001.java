0012d4dbf9c8deb97234a9f62878d803fcabd6e0
jiten228 <jiten228@yahoo.com>
2020-12-21 18:01:23 -0600

    Observe head lower right corner it should be on Branch

--- src/Jitendra_2021/Jitendra_001.java
+++ src/Jitendra_2021/Jitendra_001.java
@@ -5,5 +5,6 @@ public class Jitendra_001 {
         System.out.println("Hello Guys, new practice on github");
         System.out.println("Check head frequently");
         System.out.println("Create a new branch under your last name");
+        System.out.println("Observe head lower right corner it should be on Branch");
     }
 }
