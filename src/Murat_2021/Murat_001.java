37673d160db23399a33c5d685c91be7a2603a562
Murat Guney <muratguney@msn.com>
2020-12-21 23:01:35 -0500

    a little fixed

--- src/Murat_2021/Murat_001.java
+++ src/Murat_2021/Murat_001.java
@@ -4,7 +4,7 @@ public class Murat_001 {
 
     public static void main(String[] args) {
         System.out.println("Hello team");
-        System.out.println("How is your going?");
+        System.out.println("How is it going?");
         System.out.println("I created new branch");
         System.out.println("But I did it new branch ");
         System.out.println(" I hope, Is this way right?" );
