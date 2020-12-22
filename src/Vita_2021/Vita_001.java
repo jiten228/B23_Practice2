108583a133f23b23b3414a9735499b4e75152958
Vitakachun <kachunvita@gmail.com>
2020-12-21 20:49:03 -0600

    added awesome code

--- src/Vita_2021/Vita_001.java
+++ src/Vita_2021/Vita_001.java
@@ -3,5 +3,6 @@ package Vita_2021;
 public class Vita_001 {
     public static void main(String[] args) {
         System.out.println("Hi, everyone!");
+        System.out.println("How are you? Let's practice our new project");
     }
 }
