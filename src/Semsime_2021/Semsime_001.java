709261c723ca0832dd4d3d31d52c6faf393278ec
semsimecapraz <semsimecapraz@yahoo.com>
2020-12-23 04:06:11 -0500

    Added Branch-last commit

--- src/Semsime_2021/Semsime_001.java
+++ src/Semsime_2021/Semsime_001.java
@@ -6,7 +6,8 @@ public class Semsime_001 {
         System.out.println("Hello again!");
         System.out.println("Let's make something new");
         System.out.println("Some different things again");
-        System.out.println("And the last one");
+        System.out.println("And the  last  one");
+
 
 
 
