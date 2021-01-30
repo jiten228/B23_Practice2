cf009d34fedf6a6748f99e3a96f8023cd0ba9a58
jiten228 <jiten228@yahoo.com>
2021-01-30 09:01:01 -0600

    It's a snow day in chicago

--- src/Jitendra_2021/Test.java
+++ src/Jitendra_2021/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Hello World");
         System.out.println("Good morning! How are u?");
-
+        System.out.println("Today we expect foot of snow in chicago");
     }
 }
